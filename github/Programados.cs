f8dd019896c62c5280ff3755f951dc27e5f1fd27
21180953 <146400630+21180953@users.noreply.github.com>
2023-10-05 21:51:13 -0600

    tarea

---
+++ github/Programados.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace programados
+{
+    class programados
+    {
+        static void Main()
+        {
+            double c, f;
+            Console.WriteLine("introduce la temperatura en grados centigrados");
+            c = Convert.ToDouble(Console.ReadLine());
+            f = c * 1.8 + 32;
+                Console.WriteLine("La temperatura en grados Fahrenheit es: " + f);
+        }
+    }
+}

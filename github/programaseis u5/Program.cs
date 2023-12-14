03d67abdfc84ea2871d6afcac70abdb4be764d00
21180953 <146400630+21180953@users.noreply.github.com>
2023-12-14 17:22:31 -0600

    tarea

---
+++ github/programaseis u5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el número de términos de la secuencia de Fibonacci que desea generar: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
